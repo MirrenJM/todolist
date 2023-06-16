3680d227589c7357b4209c5c1917b4156df3668f
MirrenJM <mirrenjmacleod@gmail.com>
2023-06-16 15:29:17 +1200

    changes

--- script.js
+++ script.js
@@ -2,12 +2,16 @@ let myInput = document.querySelector(".my-input");
 let myButton = document.querySelector('button');
 let myTaskContainer = document.querySelector(".task-container")
 
-
-
-
-
 myButton.addEventListener("click",function myClick() {
-    let div = document.createElement('div');
-    myTaskContainer.appendChild(div);
+    let task = document.createElement('div');
+    task.classList.add("task-box");
+    task.innerHTML = myInput.value;
+    myTaskContainer.appendChild(task);
 })
-console.log(myButton);
+
+removeTask.addEventListener("click", function removeTaskClick(){
+    let removeTask = document.createElement ("button";)
+    
+}
+
+)
